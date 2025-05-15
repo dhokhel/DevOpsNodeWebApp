2724b32bb0e2ae1f6e381998718ee17cbbb22d18
dhokhel <dhokhkel@students.zhaw.ch>
2025-05-15 11:49:47 +0200

    update Webapp text

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Neu wird der DockerBuild jede 2 Minuten ausgeführt!');
+  res.send('Hello FS2025 DevOps Course! Neu ist der Cloud Deploy auf Render.com ');
 });
 
 app.listen(PORT, HOST);
